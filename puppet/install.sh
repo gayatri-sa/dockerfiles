75f4fc1764f8dba0f0cdd44b59ba3a2bf5a965a7
Gayatri Ajith <gayatri@schogini.com>
2021-05-27 22:52:43 +0530

    A new branch for chef only

--- puppet/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Go this solution from https://forums.docker.com/t/startup-script-after-init-in-centos-7/53483/3
-
-# script set in background
-setsid ./set-puppet-conf.sh > output.txt &
-
-# run systemd
-exec /usr/sbin/init sshd
