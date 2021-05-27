367252a2d0f55c181c0d1aaa31837dfa57aab590
Gayatri Ajith <gayatri@schogini.com>
2021-05-27 13:40:27 +0530

    Added comments

--- puppet/install.sh
+++ puppet/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Go this solution from https://forums.docker.com/t/startup-script-after-init-in-centos-7/53483/3
+
 # script set in background
 setsid ./set-puppet-conf.sh > output.txt &
 
